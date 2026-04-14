331bb7352b74045554900c0cf0cb528d9ca111e8
Isabel Rutten <isabelrutten@gmail.com>
2026-04-14 14:11:12 +0200

    Set a maximum number of tokens

--- server.js
+++ server.js
@@ -496,6 +496,7 @@ app.post("/generate", async (req, res) => {
       ],
       temperature : 0.3, 
       reasoning_effort : "none",
+      max_tokens : 2000, 
     });
 
     // Some delay due to token space
@@ -596,6 +597,7 @@ app.post("/generate", async (req, res) => {
       ],
       temperature : 0.8, 
       reasoning_effort : "none",
+      max_tokens : 2000, 
     });
 
     // 3. Output the first and second prompt together
