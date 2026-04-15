1775ed04799d1609660a5071fca960d17c56f558
Isabel Rutten <isabelrutten@gmail.com>
2026-04-15 10:11:31 +0200

    Slight decrease max tokens

--- server.js
+++ server.js
@@ -316,7 +316,7 @@ app.post("/generate", async (req, res) => {
       ],
       temperature : 0.3, 
       reasoning_effort : "none",
-      max_tokens : 1775, 
+      max_tokens : 1750, 
     });
 
     // Some delay due to token space
@@ -383,7 +383,7 @@ app.post("/generate", async (req, res) => {
       ],
       temperature : 0.8, 
       reasoning_effort : "none",
-      max_tokens : 1775, 
+      max_tokens : 1750, 
     });
 
     // 3. Output the first and second prompt together
